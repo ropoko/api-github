2eb63064104fc350e600c222823d18afd04a3f82
ropoko <rodrigostramantinoli@gmail.com>
2022-04-29 13:46:04 -0300

    feat: started header and footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<footer>
+			<p>oi sou o footer</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<header>
+			<p>oi sou o header</p>
+		</header>
+	);
+}
+
+export default Header;
