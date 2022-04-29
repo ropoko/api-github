e5d0060da46d1883e0201fff26334d586517107b
ropoko <rodrigostramantinoli@gmail.com>
2022-04-29 13:34:33 -0300

    feat: added api github connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+	const [message, setMessage] = useState('');
+
+	useEffect(() => {
+		axios
+			.get('https://api.github.com/zen')
+			.then((res) => {
+				const { data } = res;
+				setMessage(data);
+			});
+	});
+
 	return (
 		<div className="container">
-			<h1>hello world</h1>
+			<h1>{message}</h1>
 		</div>
 	);
 }
