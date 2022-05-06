e8edbb7c29e01e334c3f4214b8d7c3f27f37e2fe
ropoko <rodrigostramantinoli@gmail.com>
2022-05-06 15:33:42 -0300

    feat(favorites): finished page

--- src/components/pages/Stars/index.tsx
+++ src/components/pages/Stars/index.tsx
@@ -1,13 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Api from '../../../api';
 import { UserContext } from '../../../context/userContext';
 import './stars.style.css';
+import { Repo as RepoModel } from '../../../models/repo';
+import { getResource } from '../../../models/resource';
+import Repo from '../../Repo';
 
 function Stars() {
 	const { username } = useContext(UserContext);
+	const [response, setResponse] = useState([] as RepoModel[]);
+
+	useEffect(() => {
+		if (username === '') return;
+		const timeOutId = setTimeout(() => {
+
+			Api(getResource('favorites'), username).then(response => {
+				setResponse(response);
+			});
+
+		}, 2000);
+		return () => clearTimeout(timeOutId);
+	}, [username]);
 
 	return (
-		<main className='stars'>
-			<p>oi sou a pagina de stars - {username}</p>
+		<main className='repos'>
+			{response.map((repo) => (
+				<Repo key={repo.name} repo={repo} />
+			))}
 		</main>
 	);
 }
