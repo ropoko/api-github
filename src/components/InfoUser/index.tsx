9c88424a541261e0f20e3df072ac46373b52cc05
ropoko <rodrigostramantinoli@gmail.com>
2022-05-06 13:16:01 -0300

    fix: style on home

--- src/components/InfoUser/index.tsx
+++ src/components/InfoUser/index.tsx
@@ -4,27 +4,32 @@ import './infoUser.style.css';
 
 function InfoUser( { user } : { user: User}) {
 	return (
-		<section className='info'>
-			<div className='person'>
-				<img src={user.avatar_url} alt={`foto de ${user.name}`} />
-				<div className='about'>
-					<strong>{user.name}</strong>
-					<p>{user.bio}</p>
-					<a className='link-perfil' href={user.html_url}>Veja o perfil completo</a>
+		<>
+			{user.login !== undefined && 
+			<section className='info'>
+				<div className='person'>
+					<img src={user.avatar_url} alt={`foto de ${user.name}`} />
+					<div className='about'>
+						<strong>{user.name} - {user.login}</strong>
+						<p>{user.bio}</p>
+						<a className='link-perfil' href={user.html_url}>Veja o perfil completo</a>
+					</div>
 				</div>
-			</div>
-			<section className='statistics'>
-				<div className='group'>
-					<strong>Website: </strong> <p>{user.blog}</p>
-				</div>
-				<div className='group'>
-					<strong>followers: </strong> <p>{user.followers}</p>
-				</div>
-				<div className='group'>
-					<strong>following: </strong> <p>{user.following}</p>
-				</div>
+				<section className='statistics'>
+					<div className='group'>
+						<strong>Website: </strong> <p> <a href={`http://${user.blog}`}>{user.blog}</a> </p>
+					</div>
+					<div className='group'>
+						<strong>followers: </strong> <p>{user.followers}</p>
+					</div>
+					<div className='group'>
+						<strong>following: </strong> <p>{user.following}</p>
+					</div>
+				</section>
 			</section>
-		</section>
+			}
+		</>
+
 	);
 }
 
